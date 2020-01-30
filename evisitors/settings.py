6201269e34236a342b8872faa9fb55ded31e0ef0
rithaven <veneruth4@gmail.com>
2020-01-30 06:13:38 +0200

    Add buttons

--- evisitors/settings.py
+++ evisitors/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'evisitorsproject.apps.EvisitorsprojectConfig',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- evisitorsproject/models.py
+++ evisitorsproject/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from tinymce.models import HTMLField
+# from tinymce.models import HTMLField
 
 # Create your models here.
 
