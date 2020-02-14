46af98ed8d3a92c9fa550af157be6d4916ea460b
rithaven <veneruth4@gmail.com>
2020-02-14 15:56:33 +0200

    Add rigistration

--- evisitorsproject/models.py
+++ evisitorsproject/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django import forms
 from django.db import models
-# from django.forms import extras
-# from tinymce.models import HTMLField
+
 
 # Create your models here.
 class Idscan(models.Model):
