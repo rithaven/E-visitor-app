5c86cc750b0c2c415d3a38342ac6f8712619fe99
rithaven <veneruth4@gmail.com>
2020-02-14 16:11:34 +0200

    Add rigistration

--- evisitors/urls.py
+++ evisitors/urls.py
@@ -21,7 +21,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('evisitorsproject.urls')),
-    url(r'^tinymce/', include('tinymce.urls')),
      url(r'^accounts/',include('registration.backends.simple.urls')),
     url(r'^login/$',auth_views.login, name='login'),
     url(r'^logout/$',views.logout,{"next_page":'/'})
