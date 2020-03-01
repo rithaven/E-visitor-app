e2e0a14f5c428e3369dd9524b2c5c5a6dbe5db1a
rithaven <veneruth4@gmail.com>
2020-03-01 17:25:38 +0200

    Add Scan Button

--- evisitorsproject/views.py
+++ evisitorsproject/views.py
@@ -8,7 +8,7 @@ from django.conf import settings
 from . import models
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from imutils.video import VideoStream
+# from imutils.video import VideoStream
 from pyzbar import pyzbar
 import argparse
 import datetime
